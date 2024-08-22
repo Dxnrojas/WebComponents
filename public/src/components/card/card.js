b2b962050c6b4c83013cff34883b1a66ccbc6adf
Dxnrojas <drojasaristi@gmail.com>
2024-08-22 18:40:16 -0500

    update css

--- public/src/components/card/card.js
+++ public/src/components/card/card.js
@@ -1,34 +1,44 @@
 class Card extends HTMLElement {
-    static get observedAttributes(){
-        return ["img", "title", "description", "txtbutton"]
+    static get observedAttributes() {
+      return ["img", "title", "description", "txtbutton"];
     }
-
-    constructor(){
-        super();
-        this.attachShadow({mode: 'open'})
+  
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
     }
-
-    connectedCallback(){
-        this.render();
+  
+    connectedCallback() {
+      this.render();
     }
-
-    attributeChangedCallback(propName, oldValue, newValue){
-        
-        this[propName] = newValue;
-        this.render();
+  
+    attributeChangedCallback(propName, oldValue, newValue) {
+      this[propName] = newValue;
+      this.render();
     }
-
-    render (){
-        this.shadowRoot.innerHTML= `
-        <link rel="stylesheet" href="./src/components/card/card.css">
-       <div class="product-card">
-       <img src="${this.img}" alt="">
-        <h1>${this.title}</h1>
-        <p>${this.description}</p> 
-        <button>${this.txtbutton}</button>
-       </div> `
+  
+    render() {
+      this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="./src/components/card/card.css">
+      <section class="container">
+  
+      <div class= "product-img">
+      <img src="${this.img}" alt="">
+      </div> 
+  
+      <div class= "product-text">
+      <h1>${this.title}</h1>
+      <p>${this.description}</p> 
+      <button>${this.txtbutton}</button>
+      </div>
+     
+      
+      </section>
+      
+     `;
     }
-}
-
-customElements.define('product-card', Card)
-export default Card
+  }
+  
+  customElements.define("product-card", Card);
+  export default Card;
+  
